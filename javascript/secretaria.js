b4c6d8b154e73519130fb6ad5433a87c447616b4
EMPauloFreire <157525943+EMPauloFreire@users.noreply.github.com>
2024-02-01 18:14:19 -0300

    Add files via upload

---
+++ javascript/secretaria.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Exibir o conteúdo inicial quando a página é carregada
+    mostrarConteudo('conteudoInicial');
+  });
+  
+  function mostrarConteudo(idConteudo, elementoSelecionado) {
+    // Oculta todos os conteúdos
+    const todosConteudos = document.querySelectorAll('.conteudo');
+    todosConteudos.forEach(conteudo => {
+      conteudo.style.display = 'none';
+    });
+  
+    // Remove a classe 'selecionado' de todos os itens do menu
+    const todosItensMenu = document.querySelectorAll('.opcoes li');
+    todosItensMenu.forEach(item => {
+      item.classList.remove('selecionado');
+    });
+  
+    // Adiciona a classe 'selecionado' apenas ao item clicado
+    if (elementoSelecionado) {
+      elementoSelecionado.classList.add('selecionado');
+    }
+  
+    // Exibe o conteúdo correspondente à opção selecionada
+    const conteudoSelecionado = document.getElementById(idConteudo);
+    conteudoSelecionado.style.display = 'block';
+  }
